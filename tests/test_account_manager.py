ef01968be0fde8af55f1ac363ebeb1c2d3437c67
David Andersson <lejonmanen@gmail.com>
2026-02-23 15:23:46 +0100

    rött testfall

--- tests/test_account_manager.py
+++ tests/test_account_manager.py
@@ -1,16 +1,24 @@
-from bank_account import BankAccount
+#from bank_account import BankAccount
+from account_manager import calculate_interest
 
 
 def test_calculate_interest():
     # arrange
-    account = BankAccount()
-    initial = account.get_balance()
+    account = FakeAccount()
+    initial = account.balance
     after_interest = initial * 1.05
 
     # act
     calculate_interest(account)
 
     # assert
-    actual = account.get_balance()
+    actual = account.balance
     assert actual == after_interest
 
+
+class FakeAccount:
+    def __init__(self):
+        self.balance = 2000   # 5% är 100 kr
+
+    def set_balance(self, new_balance):
+        self.balance = new_balance
